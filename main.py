2330f6a0263c9e6d39aeedfbc63962b7eb20f72b
David <davibloch@googlemail.com>
2019-05-04 13:37:24 +0200

    Verbesserter Algorithmus
    ImageBluring und danach CannyFilter
    Daraus wird eine Convexe Hülle berechnet. Das ist unser Objekt.
    Outputbilder als Ergebnis.

--- main.py
+++ main.py
@@ -6,62 +6,101 @@ Created on Thu Apr 18 15:06:43 2019
 """
 import numpy as np
 import cv2
+import random as rng
+import glob
 
 # definiere Farb-Ranges
 lower_black = (0, 0, 40)
 upper_black = (180, 255, 255)
 
-threshold = 50
+point = (150, 150)
+
+threshold = 70
+
+do_live = False # Schalter zwischen LiveKamera und Übungsbildern
+#do_live = True
+  
+
+def detect(frame):    
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+        
+
+    frame = cv2.blur(frame,(3,3))
+    
+    # Nutze Canny Filter zum detektieren von Kanten
+    edges = cv2.Canny(frame, 100, 255)
+
+    # finde Konturen
+    _, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    hull_list = []
+    for i in range(len(contours)):
+        hull = cv2.convexHull(contours[i])
+        hull_list.append(hull)
+        
+    cont = np.vstack(contours[i] for i in range(len(contours)))
+    hull_all = []
+    
+    hull_all.append(cv2.convexHull(cont))
+    
+    # Draw contours + hull results
+    #frame = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
+    for i in range(len(contours)):
+        color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+        #cv2.drawContours(frame, hull_list, i, color)
+            
+    cv2.imshow(file, edges) #Zeigt die Maske als Bild.
+
+    if len(contours) > 0:
+        objekt = max(hull_all, key=cv2.contourArea)
+ 
+        # zeichne die Bounding box des Tennisballs in das Video-Bild ein:
+        x, y, w, h = cv2.boundingRect(objekt)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=3)
+        cv2.putText(frame,"Width: {}, Height: {}".format(w, h),(1,100), font, 0.5,(int(155),int(155),int(155)),2,cv2.LINE_AA)
+        
+        #rect = cv2.minAreaRect(cnt)
+        #box = cv2.boxPoints(rect)
+        #box = np.int0(box)
+        #cv2.drawContours(frame,[box],0,(0,255,255),2)
+    return frame
 
 
 if __name__ == "__main__":
-    point = (150, 150)
-    cap = cv2.VideoCapture(0)
+    
+    cnt = 0
+    fnames = glob.glob("_Data/*.jpg")
+    
+    if do_live:
+        cap = cv2.VideoCapture(0); #Initialisiere die Kamera
    
-    while True:
-        ret, frame = cap.read()
-        if ret == True: 
-            #color = frame[150, 150]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            #cv2.putText(frame,'Classifier VdKI',(1,50), font, 2,(0,255,0),2,cv2.LINE_AA)
-            #cv2.circle(frame,point,10,(0,255,0))   
-            #cv2.putText(frame,str(color),(1,100), font, 0.5,(int(color[0]),int(color[1]),int(color[2])),2,cv2.LINE_AA)
+    while True: #Schleife 
+        
+        if do_live:
+            ret, frame = cap.read()
+        else:
+            ret = True
+            print(fnames[cnt])
+            frame = cv2.imread(fnames[cnt])
             
-            mask = cv2.inRange(frame, lower_black, upper_black)
-           
-            im2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        if ret == True: # Falls gültiges Bild gelesen
+            frame = detect(frame)
             
-            _, im2 = cv2.threshold(im2, threshold, 255, cv2.THRESH_BINARY)
+            cv2.imshow(str(fnames[cnt] + "orig"), frame)
             
-                        # finde Konturen in der Maske, die nur noch zeigt, wo gelbe Pixel sind:
-            _, contours, _ = cv2.findContours(im2, cv2.RETR_EXTERNAL,
-                                                  cv2.CHAIN_APPROX_SIMPLE)
-            
-            
-            cv2.drawContours(im2, contours, -1, (0,255,0))
-            cv2.imshow("im2", im2) #Zeigt die Maske als Bild.
-
-
-            if len(contours) > 0:
-                objekt = max(contours, key=cv2.contourArea)
-         
-                # zeichne die Bounding box des Tennisballs in das Video-Bild ein:
-                x, y, w, h = cv2.boundingRect(objekt)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=3)
-                cv2.putText(frame,"Width: {}, Height: {}".format(w, h),(1,100), font, 0.5,(int(155),int(155),int(155)),2,cv2.LINE_AA)
-                
-                #rect = cv2.minAreaRect(cnt)
-                #box = cv2.boxPoints(rect)
-                #box = np.int0(box)
-                #cv2.drawContours(frame,[box],0,(0,255,255),2)
-
-            cv2.imshow("frame", frame)
+            cv2.imwrite("C:/Users/David/Documents/GitHub/HSKa_vdki2019/_Data/Puit/{}.jpg".format(fnames[cnt][6:10]), frame);
             
             if cv2.waitKey(20) & 0xFF == ord("q"):
                 break
             
+            cnt = cnt + 1
+            if do_live == False and cnt >= len(fnames):
+                break
+
             
             
-        else:
+        else: # Falls Bild ungültig, Kamera nicht bereit oÄ
             print("shit")
             break
