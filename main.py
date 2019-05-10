1cca3ca71fdaf0f36b480e7d45fe70d2e62ef652
maurusGR <maurus.manatschal@gmail.com>
2019-05-10 14:43:09 +0200

    minArearect

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ import glob
 lower_black = (0, 0, 40)
 upper_black = (180, 255, 255)
 
+use_tilted_rect = True
+
 point = (150, 150)
 
 threshold = 70
 
-do_live = False # Schalter zwischen LiveKamera und Übungsbildern
-#do_live = True
+#do_live = False # Schalter zwischen LiveKamera und Übungsbildern
+do_live = True
   
 
 def detect(frame):    
@@ -48,33 +50,44 @@ def detect(frame):
     #frame = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
     for i in range(len(contours)):
         color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-        #cv2.drawContours(frame, hull_list, i, color)
+        cv2.drawContours(frame, hull_list, i, color)
             
-    cv2.imshow(file, edges) #Zeigt die Maske als Bild.
+    cv2.imshow('file', edges) #Zeigt die Maske als Bild.
 
     if len(contours) > 0:
         objekt = max(hull_all, key=cv2.contourArea)
  
-        # zeichne die Bounding box des Tennisballs in das Video-Bild ein:
-        x, y, w, h = cv2.boundingRect(objekt)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=3)
-        cv2.putText(frame,"Width: {}, Height: {}".format(w, h),(1,100), font, 0.5,(int(155),int(155),int(155)),2,cv2.LINE_AA)
-        
-        #rect = cv2.minAreaRect(cnt)
-        #box = cv2.boxPoints(rect)
-        #box = np.int0(box)
-        #cv2.drawContours(frame,[box],0,(0,255,255),2)
+        if use_tilted_rect:
+            rect = cv2.minAreaRect(objekt)
+            box = cv2.boxPoints(rect)
+           
+            box = np.intp(box)
+            
+            _, (w,h), _ = rect;
+
+            cv2.drawContours(frame,[box],0,(0,0,255),2)
+
+            cv2.putText(frame,"Width: {:f}, Height: {:f}".format(w, h),(1,100), font, 0.5,(int(155),int(155),int(155)),2,cv2.LINE_AA)
+
+        else:    
+            # zeichne die Bounding box des Tennisballs in das Video-Bild ein:
+            x, y, w, h = cv2.boundingRect(objekt)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=3)
+            cv2.putText(frame,"Width: {}, Height: {}".format(w, h),(1,100), font, 0.5,(int(155),int(155),int(155)),2,cv2.LINE_AA)
+
     return frame, edges
 
 
 if __name__ == "__main__":
     
     cnt = 0
-    fnames = glob.glob("_Data/*.jpg")
+    
     
     if do_live:
-        cap = cv2.VideoCapture(0); #Initialisiere die Kamera
-   
+        cap = cv2.VideoCapture(1); #Initialisiere die Kamera
+    else:
+        fnames = glob.glob("_Data/*.jpg")
+    
     while True: #Schleife 
         
         if do_live:
@@ -88,10 +101,10 @@ if __name__ == "__main__":
         if ret == True: # Falls gültiges Bild gelesen
             frame, edges  = detect(frame)
             
-            cv2.imshow(str(fnames[cnt] + "orig"), frame)
+            cv2.imshow(str("orig"), frame)
             
-            cv2.imwrite("C:/Users/David/Documents/GitHub/HSKa_vdki2019/_Data/Puit/{}.jpg".format(fnames[cnt][6:10]), frame);
-            cv2.imwrite("C:/Users/David/Documents/GitHub/HSKa_vdki2019/_Data/Puit/{}_canny.jpg".format(fnames[cnt][6:10]), edges);
+            #cv2.imwrite("C:/Users/David/Documents/GitHub/HSKa_vdki2019/_Data/Puit/{}.jpg", frame);
+            #cv2.imwrite("C:/Users/David/Documents/GitHub/HSKa_vdki2019/_Data/Puit/{}_canny.jpg".format(fnames[cnt][6:10]), edges);
             
             if cv2.waitKey(20) & 0xFF == ord("q"):
                 break
