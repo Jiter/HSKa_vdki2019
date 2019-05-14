2006507e4f37e7966d7bc3141f7c16268c94e06b
David <davibloch@googlemail.com>
2019-05-14 10:21:04 +0200

    PEP8

--- main.py
+++ main.py
@@ -19,29 +19,29 @@ point = (150, 150)
 
 threshold = 70
 
-#do_live = False # Schalter zwischen LiveKamera und Übungsbildern
+# do_live = False # Schalter zwischen LiveKamera und Übungsbildern
 do_live = True
-  
 
-def detect(frame):    
+
+def detect(frame):
 
     font = cv2.FONT_HERSHEY_SIMPLEX
-        
 
-    frame = cv2.blur(frame,(3,3))
-    
+    frame = cv2.blur(frame, (3, 3))
+
     # Nutze Canny Filter zum detektieren von Kanten
     edges = cv2.Canny(frame, 100, 255)
 
     # finde Konturen
-    _, contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
+    _, contours, _ = cv2.findContours(edges, cv2.RETR_TREE,
+                                      cv2.CHAIN_APPROX_SIMPLE)
+
     hull_list = []
     for i in range(len(contours)):
         hull = cv2.convexHull(contours[i])
         hull_list.append(hull)
-    
-    #if ():    
+
+    # if ():    
     cont = np.vstack(contours[i] for i in range(len(contours)))
     hull_all = []
     
