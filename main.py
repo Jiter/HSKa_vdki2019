cc95295386a5fd50efe0a6b2cce14c32e1e79ff5
David <davibloch@googlemail.com>
2019-05-14 17:00:49 +0200

    Prepared Functions to Calculate Probabilitys from our Classes

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import cv2
 import random as rng
 import glob
+import math
 
 use_tilted_rect = True
 
@@ -42,19 +43,19 @@ def detect(frame):
         hull_all = []
 
         hull_all.append(cv2.convexHull(cont))
-        
+
         mask = edges
-        
+
         cv2.drawContours(mask, hull_all, -1, (255, 255, 255), -1)
         # cv2.imshow("Mask", mask)
-        
+
         b, g, r, _ = np.uint8(cv2.mean(frame, mask))
-        
+
         cv2.putText(frame, "Color: {},{},{}".format(b, g, r),
-            (10, 40), font, 0.5,
-            (int(b), int(g), int(r)),
-            2, cv2.LINE_AA)
-        
+                    (10, 40), font, 0.5,
+                    (int(b), int(g), int(r)),
+                    2, cv2.LINE_AA)
+
         # Draw contours + hull results
         # frame = np.zeros((edges.shape[0], edges.shape[1], 3), dtype=np.uint8)
         for i in range(len(contours)):
@@ -63,14 +64,13 @@ def detect(frame):
                      rng.randint(0, 256))
 
         print('{:d},{:d},{:d}'.format(b, g, r))
-            #cv2.drawContours(frame, hull_list, i, color)
+            # cv2.drawContours(frame, hull_list, i, color)
 
         cv2.imshow('Canny', edges)  # Zeigt die Maske als Bild.
 
         if len(contours) > 0:
             objekt = max(hull_all, key=cv2.contourArea)
-            
-            
+
             if use_tilted_rect:
                 rect = cv2.minAreaRect(objekt)
                 box = cv2.boxPoints(rect)
@@ -113,7 +113,6 @@ def detect(frame):
                             (10, 20), font, 0.5, (int(155), int(155),
                             int(155)), 2, cv2.LINE_AA)
 
-
     return frame, edges
 
 
@@ -128,47 +127,72 @@ def makeWindows():
     cv2.waitKey(10)
 
 
-def thisIsWhereTheMagicHappens(w, h):
-            # Klasse, Schmetterling, Hase, Schaf, Küken
-    prob_h = np.array([
-            [0,     0,      0.16,   0,      0],     # 290 - 295
-            [1,     0,      0.595,  0,      0],     # 295 - 300
-            [2,     0.093,  0.238,  0,      0],     # 300 - 305
-            [3,     0.240,  0,      0,      0],     # 305 - 310
-            [4,     0.2,    0,      0,      0],     # 310 - 315
-            [5,     0.053,  0,      0,      0],     # 315 - 320
-            [6,     0,      0,      0,      0],     # 320 - 325
-            [7,     0.253,  0,      0,      0],     # 325 - 330
-            [8,     0.16,   0,      0.079,  0],     # 330 - 335
-            [9,     0,      0,      0.571,  0],     # 335 - 340
-            [11,    0,      0,      0.19,   0],     # 340 - 345
-            [12,    0,      0,      0.158,  0],     # 345 - 350
-            [13,    0,      0,      0,      0],     # 350 - 355
-            [14,    0,      0,      0,      0],     # 355 - 360
-            [15,    0,      0,      0,      0],     # 360 - 365
-            [16,    0,      0,      0,      0],     # 365 - 370
-            [17,    0,      0,      0,      0],     # 370 - 375
-            [18,    0,      0,      0,      0],     # 375 - 380
-            [19,    0,      0,      0,      0.067], # 380 - 385
-            [20,    0,      0,      0,      0.284], # 385 - 390
-            [21,    0,      0,      0,      0.243], # 390 - 395
-            [22,    0,      0,      0,      0.365], # 395 - 400
-            [23,    0,      0,      0,      0.040], # 400 - 405
+def loadProbabilityArrays(h):
+    prob = []
+    #       Klasse, Schmetterling, Hase, Schaf, Küken
+    prob = np.array([
+            [0,     0,      0.16,   0,      0],      # 290 - 295
+            [1,     0,      0.595,  0,      0],      # 295 - 300
+            [2,     0.093,  0.238,  0,      0],      # 300 - 305
+            [3,     0.240,  0,      0,      0],      # 305 - 310
+            [4,     0.2,    0,      0,      0],      # 310 - 315
+            [5,     0.053,  0,      0,      0],      # 315 - 320
+            [6,     0,      0,      0,      0],      # 320 - 325
+            [7,     0.253,  0,      0,      0],      # 325 - 330
+            [8,     0.16,   0,      0.079,  0],      # 330 - 335
+            [9,     0,      0,      0.571,  0],      # 335 - 340
+            [10,    0,      0,      0.19,   0],      # 340 - 345
+            [11,    0,      0,      0.158,  0],      # 345 - 350
+            [12,    0,      0,      0,      0],      # 350 - 355
+            [13,    0,      0,      0,      0],      # 355 - 360
+            [14,    0,      0,      0,      0],      # 360 - 365
+            [15,    0,      0,      0,      0],      # 365 - 370
+            [16,    0,      0,      0,      0],      # 370 - 375
+            [17,    0,      0,      0,      0],      # 375 - 380
+            [18,    0,      0,      0,      0.067],  # 380 - 385
+            [19,    0,      0,      0,      0.284],  # 385 - 390
+            [20,    0,      0,      0,      0.243],  # 390 - 395
+            [21,    0,      0,      0,      0.365],  # 395 - 400
+            [22,    0,      0,      0,      0.040],  # 400 - 405
             ])
-    
-    
+
+    return prob[height2Class(h), :]
+
+
+# Function to calculate the given Class from the Height
+def height2Class(h):
+    c = math.floor(((h - 290) / 5))
+    if c > 22:
+        c = 22
+    elif c < 0:
+        c = 0
+
+    return c
+
+# Calculate the Probability for all Classes on one Height-Class
+def calcProb(prob, klasse):
+    return ((prob[klasse]) / 4) / ((prob[1] + prob[2] + prob[3] + prob[4]) / 4)
+
+
+# Give Back an Vector with probabilitys of each Teached Class
+def probabilityMatrix(classprob):
+    v = []
+    for i in range(4):
+        v.append(calcProb(classprob, i + 1))
+    return v
+
+
+def thisIsWhereTheMagicHappens(h):
+    classprob = loadProbabilityArrays(h)
+    heightprob = probabilityMatrix(classprob)
+
+    print(heightprob)
 
 
 if __name__ == "__main__":
 
     cnt = 0
 
-#    for i in range(1,1499) :
-#        cap = cv2.VideoCapture(i)
-#        if (cap.isOpened()):
-#            print("Found camera {}\n".format(i))
-#            break;
- 
     makeWindows()
 
     if do_live:
@@ -198,7 +222,7 @@ if __name__ == "__main__":
 
             if (not do_live) and cnt >= len(fnames):
                 break
-            
+
             cnt = cnt + 1
             if cnt > 11:
                 break
